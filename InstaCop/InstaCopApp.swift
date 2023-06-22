4ffcd8e2e5371c608c0b162b84fd1d535dc696b3
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-06-22 14:59:43 -0500

    Initial Commit

---
+++ InstaCop/InstaCopApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaCopApp.swift
+//  InstaCop
+//
+//  Created by Kevin Candia Villagómez on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaCopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
