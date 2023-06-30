817b185fa6fbc64f4717a92ee927cc6f25d97db1
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-06-30 01:04:33 -0500

    fixing view presentation and Post model with mockData

--- InstaCop/Core/Profile/CurrentUserProfileView.swift
+++ InstaCop/Core/Profile/CurrentUserProfileView.swift
@@ -1,28 +1,28 @@
 //
-//  ProfileView.swift
+//  CurrentUserProfileView.swift
 //  InstaCop
 //
-//  Created by Kevin Candia Villagómez on 22/06/23.
+//  Created by Kevin Candia Villagómez on 29/06/23.
 //
 
 import SwiftUI
 
-struct ProfileView: View {
-    // MARK: - Input user data
-    let user: User
+struct CurrentUserProfileView: View {
     // MARK: - Input for LazyGrid
     private let gridItems: [GridItem] = [
         .init(.flexible(),spacing: 1),
         .init(.flexible(),spacing: 1),
         .init(.flexible(),spacing: 1)
     ]
+    
     var body: some View {
+        NavigationStack {
             ScrollView {
                 // MARK: - Hearder
                 VStack(spacing: 10) {
                     // MARK: - pic and stats
                     HStack {
-                        Image(user.profileImageUrl ?? "")
+                        Image("profileSP")
                             .resizable()
                             .scaledToFill()
                             .frame(width: 80,height: 80)
@@ -43,17 +43,12 @@ struct ProfileView: View {
                     
                     // MARK: - name and bio
                     VStack(alignment: .leading, spacing: 4) {
-                        if let fullname = user.fullname {
-                            Text(fullname)
-                                .font(.footnote)
-                                .fontWeight(.semibold)
-                        }
+                        Text("Totti Candia")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
                         
-                        if let bio = user.bio {
-                            Text(bio)
-                                .font(.footnote)
-                        }
-                        
+                        Text("Lima, Peru")
+                            .font(.footnote)
                     }
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.horizontal)
@@ -73,7 +68,7 @@ struct ProfileView: View {
                     
                     Divider()
                 }
-                // MARK: - Post Grid view 
+                // MARK: - Post Grid view
                 
                 LazyVGrid(columns: gridItems, spacing: 2) {
                     ForEach(0 ... 15, id: \.self) { index in
@@ -86,11 +81,21 @@ struct ProfileView: View {
             }
             .navigationTitle("Profile")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundColor(.black)
+                    }
+                }
+            }
+        }
     }
 }
 
-struct ProfileView_Previews: PreviewProvider {
+struct CurrentUserProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView(user: User.MOCK_USERS[0])
+        CurrentUserProfileView()
     }
 }
---
+++ InstaCop/Model/Post.swift
@@ -0,0 +1,51 @@
+//
+//  Post.swift
+//  InstaCop
+//
+//  Created by Kevin Candia Villagómez on 30/06/23.
+//
+
+import Foundation
+
+struct Post: Identifiable, Hashable, Codable {
+    let id: String
+    let ownerUid: String
+    let caption: String
+    var likes: Int
+    let imageUrl: String
+    let timeStamp: Date
+    var user: User?
+}
+
+extension Post {
+    static var MOCK_POST: [Post] = [
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "This is some test caption for now",
+              likes: 123,
+              imageUrl: "batman-2",
+              timeStamp: Date(),
+              user: User.MOCK_USERS[0]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Wakanda forever",
+              likes: 104,
+              imageUrl: "blackpanther-2",
+              timeStamp: Date(),
+              user: User.MOCK_USERS[1]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Iron Man",
+              likes: 12,
+              imageUrl: "ironman-2",
+              timeStamp: Date(),
+              user: User.MOCK_USERS[2]),
+        .init(id: NSUUID().uuidString,
+              ownerUid: NSUUID().uuidString,
+              caption: "Venom is hungry. Time to eat",
+              likes: 314,
+              imageUrl: "venom-2",
+              timeStamp: Date(),
+              user: User.MOCK_USERS[3]),
+    ]
+}
