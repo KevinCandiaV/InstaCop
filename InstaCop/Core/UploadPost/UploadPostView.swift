61f551655ca274b78b30c6a4f87bf142b0305681
Kevin Candia Villagómez <kevin.candia@softtek.com>
2023-07-09 10:27:09 -0500

    adding uploadPostViewModel

--- InstaCop/Core/UploadPost/UploadPostView.swift
+++ InstaCop/Core/UploadPost/UploadPostView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct UploadPostView: View {
     @State private var caption = ""
+    @State private var imagePickerPresented = false
+    @StateObject var viewModel = UploadPostViewModel()
+    
     var body: some View {
         VStack {
             // MARK: - Action tool bar
             HStack {
                 Button {
                     print("Cancel upload")
+                    caption = ""
+                    viewModel.selectedImage = nil
+                    viewModel.postImage = nil
                 } label: {
                     Text("Cancel")
                 }
@@ -35,18 +41,29 @@ struct UploadPostView: View {
 
             }
             .padding(.horizontal)
+            
             // MARK: - Post image and caption
             HStack(spacing: 8) {
-                Image("batmanPost1")
-                    .resizable()
-                    .frame(width: 100, height: 100)
                 
-                TextField("Enter your caption...", text: $caption, axis: .vertical)
+                if let image = viewModel.postImage {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 100, height: 100)
+                        .clipped()
+                }
+                
+                TextField("Enter your caption ...", text: $caption, axis: .vertical)
                 
             }
+            .padding()
             
             Spacer()
         }
+        .onAppear {
+            imagePickerPresented.toggle()
+        }
+        .photosPicker(isPresented: $imagePickerPresented, selection: $viewModel.selectedImage)
     }
 }
 
